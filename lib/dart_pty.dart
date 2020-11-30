0e400ecb2826ddc7ed017f9aef737e27b6809a8a
Nightmare-MY <906262255@qq.com>
2020-11-30 14:40:24 +0800

    Update dart_pty.dart

--- lib/dart_pty.dart
+++ lib/dart_pty.dart
@@ -2,9 +2,3 @@ library dart_pty;
 
 export 'src/unix_pty.dart';
 export 'src/unix_pty_c.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
