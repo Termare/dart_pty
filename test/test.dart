9b3f3022854dcb2c46ecf19f6823d8c212e77f05
Nightmare-MY <906262255@qq.com>
2020-12-28 09:35:00 +0800

    format code

--- test/test.dart
+++ test/test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:dart_pty/src/pseudo_terminal.dart';
-import 'package:dart_pty/src/unix_pty_c.dart';
 
 Future<void> main() async {
   final Map<String, String> environment = {'TEST': 'TEST_VALUE'};
@@ -14,20 +13,20 @@ Future<void> main() async {
     environment: environment,
   );
   print('pseudoTerminal -> ${pseudoTerminal.getTtyPath()}');
-  await Future<void>.delayed(Duration(milliseconds: 100));
+  await Future<void>.delayed(const Duration(milliseconds: 100));
   String result;
   print('第一次进程的输出为:${pseudoTerminal.readSync()}');
-  await Future.delayed(Duration(milliseconds: 100), () async {
+  await Future.delayed(const Duration(milliseconds: 100), () async {
     while (true) {
       print('请向终端输入一些东西');
-      String input = stdin.readLineSync();
+      final String input = stdin.readLineSync();
       pseudoTerminal.write(input + '\n');
-      await Future<void>.delayed(Duration(milliseconds: 200));
+      await Future<void>.delayed(const Duration(milliseconds: 200));
       result = pseudoTerminal.readSync();
       print('\x1b[31m' + '-' * 20 + 'result' + '-' * 20);
       print('result -> $result');
       print('-' * 20 + 'result' + '-' * 20 + '\x1b[0m');
-      await Future<void>.delayed(Duration(milliseconds: 100));
+      await Future<void>.delayed(const Duration(milliseconds: 100));
     }
   });
 }
