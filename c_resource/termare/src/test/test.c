7986cf18a15120a07a29e906f811c007e43fb534
Nightmare-MY <906262255@qq.com>
2021-01-13 16:57:53 +0800

    sync macos code

--- c_resource/termare/src/test/test.c
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
-{
-    MessageBox(NULL, "Hello world!", "Title", MB_OK);
-    return 0;
-}
