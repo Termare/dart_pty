372d298c63b27ff99d19261d3996f0c1bae02f3a
Nightmare-MY <906262255@qq.com>
2021-05-11 18:49:20 +0800

    add ffigen

---
+++ ffigen/term/header/term.h
@@ -0,0 +1,19 @@
+
+int create_ptm(
+    int rows,
+    int columns);
+void create_subprocess(char *env,
+                       char const *cmd,
+                       char const *cwd,
+                       char *const argv[],
+                       char **envp,
+                       int *pProcessId,
+                       int ptmfd);
+void write_to_fd(int fd, char *str);
+void setNonblock(int fd);
+char *get_output_from_fd(int fd);
+char *getFilePathFromFd(int fd);
+typedef void (*Callback)(char *p);
+void setPtyWindowSize(int fd, int rows, int cols);
+void init_dart_print(Callback callback);
+void post_thread(int ptmfd, Callback callback);
