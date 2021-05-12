c0a41ba40e9abb5b5ac9b3bb345fcf6facabe65f
Nightmare-MY <906262255@qq.com>
2021-05-12 14:17:10 +0800

    Update test.dart

--- test/test.dart
+++ test/test.dart
@@ -13,26 +13,29 @@ Future<void> main() async {
     executable: executable,
     environment: environment,
     libPath: './dynamic_library/libterm.dylib',
+    workingDirectory: '/',
   );
   print('pseudoTerminal -> ${pseudoTerminal.getTtyPath()}');
   await Future<void>.delayed(const Duration(milliseconds: 100));
-  String result = utf8.decode(await pseudoTerminal.read());
-  print('第一次进程的输出为:${result}');
-  // return;
-  // await Future.delayed(const Duration(milliseconds: 100), () async {
-  //   while (true) {
-  //     print('请向终端输入一些东西');
-  //     final String input = stdin.readLineSync()!;
-  //     input.split('').forEach((element) {
-  //       pseudoTerminal.write(element);
-  //     });
-  //     pseudoTerminal.write('\n');
-  //     await Future<void>.delayed(const Duration(milliseconds: 200));
-  //     result = utf8.decode(await pseudoTerminal.read());
-  //     print('\x1b[31m' + '-' * 20 + 'result' + '-' * 20);
-  //     print('result -> $result');
-  //     print('-' * 20 + 'result' + '-' * 20 + '\x1b[0m');
-  //     await Future<void>.delayed(const Duration(milliseconds: 100));
-  //   }
-  // });
+
+  final String result = utf8.decode(await pseudoTerminal.read());
+  print('第一次进程的输出为:$result');
+  pseudoTerminal.out!.transform(utf8.decoder).listen((line) {
+    print('\x1b[31m' + '-' * 20 + 'result' + '-' * 20);
+    print('\x1b[32m$line\x1b[31m');
+    print('-' * 20 + 'result' + '-' * 20 + '\x1b[0m');
+  });
+  pseudoTerminal.startPolling();
+  await Future.delayed(const Duration(milliseconds: 100), () async {
+    while (true) {
+      print('请向终端输入一些东西');
+      final String input = stdin.readLineSync()!;
+      input.split('').forEach((element) {
+        pseudoTerminal.write(element);
+      });
+      pseudoTerminal.write('\n');
+
+      await Future<void>.delayed(const Duration(milliseconds: 100));
+    }
+  });
 }
