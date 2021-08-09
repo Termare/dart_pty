bea3710cdab5a00df162cefb8f00336229ad1784
Nightmare-MY <906262255@qq.com>
2021-08-09 23:20:22 +0800

    add setNonblock impl

--- lib/src/foundation/file_descriptor.dart
+++ lib/src/foundation/file_descriptor.dart
@@ -1,8 +1,10 @@
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:dart_pty/src/native_header/generated_bindings.dart';
 import 'package:ffi/ffi.dart';
+import 'package:signale/signale.dart';
 
 class FileDescriptor {
   const FileDescriptor(this.fd, this.nativeLibrary);
@@ -19,5 +21,16 @@ class FileDescriptor {
     } else {
       return resultPoint.asTypedList(len);
     }
+  }
+
+  void setNonblock(int fd, {bool verbose = true}) {
+    int flag = -1;
+    flag = nativeLibrary.fcntl(fd, F_GETFL, 0); //获取当前flag
+    Log.d('> 当前flag = $flag');
+    flag |= Platform.isAndroid ? O_NONBLOCK_ANDROID : O_NONBLOCK; //设置新falg
+    Log.d('> 设置新flag = $flag');
+    nativeLibrary.fcntl(fd, F_SETFL, flag); //更新flag
+    flag = nativeLibrary.fcntl(fd, F_GETFL, 0); //获取当前flag
+    Log.d('> 再次获取到的flag = $flag');
   }
 }
